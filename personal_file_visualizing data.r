4e24b88256d923d4dc6acc1dd2b90826327a1dc6
rodrigoacastro <webyard1@yahoo.com.br>
2019-02-22 11:15:35 +1100

    new plot

--- personal_file_visualizing data.r
+++ personal_file_visualizing data.r
@@ -1,2 +1,23 @@
 # Visualizing data
 
+# dplyr and tidyr
+
+library(tidyverse)
+
+getwd()
+dir()
+
+interviews_plotting <- read_csv("data_output/interviews_plotting.csv")
+
+ggplot(data=interviews_plotting)
+
+ggplot(data=interviews_plotting,
+       aes (x=no_membrs,y=number_items)) +
+	geom_point()
+       
+
+interview_plot + 
+	geom_line()
+
+interview_plot +
+	geom_bar()
--- scripts/workshop.R
+++ scripts/workshop.R
@@ -3,3 +3,22 @@
 ## If you want to save plots you created, place them in the 'figures/' directory.
 ## You can create additional R files in the 'scripts/' directory.
 
+library(tidyverse)
+
+getwd()
+dir()
+
+interviews_plotting <- read_csv("data_output/interviews_plotting.csv")
+
+ggplot(data=interviews_plotting)
+
+ggplot(data=interviews_plotting,
+       aes (x=no_membrs,y=number_items)) +
+  geom_point()
+
+
+interview_plot + 
+  geom_line()
+
+interview_plot +
+  geom_bar()
