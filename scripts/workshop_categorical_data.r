15faa461add420bfa3fe82f3fcc86df9827545da
rodrigoacastro <webyard1@yahoo.com.br>
2019-02-22 14:09:30 +1100

    adds graphs and splits data into sets

--- scripts/workshop_categorical_data.r
+++ scripts/workshop_categorical_data.r
@@ -7,22 +7,17 @@ dir()
 
 interviews_plotting <- read_csv("data_output/interviews_plotting.csv")
 
+# scatter plots
 ggplot(data=interviews_plotting,
   aes (x=no_membrs,y=number_items)) +
   geom_point(alpha=0.5)
 )
 
 
-
+# bar plots
 ggplot(data=interviews_plotting,
        aes (x=respondent_wall_type,fill=village)) +
   geom_bar(position="dodge")
-)
-
-?read_csv
-
-
-interviews_plotting
 
 
 
@@ -31,7 +26,7 @@ interviews_plotting
 ggplot(data=interviews_plotting,
        aes (x=village,fill=village)) +
   geom_bar(position="dodge") + facet_wrap(~ respondent_wall_type)
-)
+
 
 # 4 graphs in 1
 
@@ -90,4 +85,54 @@ ggplot(data = interviews_plotting,
        aes(x=respondent_wall_type, y = rooms)) +
   geom_boxplot() +
   geom_jitter(alpha=0.5,width = 0.2, height = 0.2, color = "tomato")
+
+ggplot(data = interviews_plotting,
+       aes(x=respondent_wall_type, y = rooms)) +
+  geom_boxplot() +
+  geom_jitter(alpha=0.5,width = 0.2, height = 0.2, aes(color = village))
+
+
+# adding colors
+ggplot(data = interviews_plotting,
+       aes(x=respondent_wall_type, y = rooms,fill=village)) +
+  geom_boxplot() +
+  geom_jitter(alpha=0.5,width = 0.2, height = 0.2, color = "tomato")
+
+
+
+ggplot(data = interviews_plotting,
+       aes(x=respondent_wall_type, y = rooms,color=village)) +
+  geom_boxplot() +
+  geom_jitter(alpha=0.5,width = 0.2, height = 0.2, color = "tomato")
+
+
+
+ggplot(data = interviews_plotting,
+       aes(x=respondent_wall_type, y = liv_count)) +
+  geom_boxplot() +
+  geom_jitter(alpha=0.5,width = 0.2, height = 0.2, 
+  aes(color = memb_assoc))
+
+
+# separates points per boxplot
+ggplot(data = interviews_plotting,
+       aes(x=respondent_wall_type, y = liv_count,
+           fill=memb_assoc, color = memb_assoc)) +
+  geom_boxplot(alpha=0.5) +
+  geom_point(alpha = 0.5,
+             position = position_jitterdodge(jitter.width = 0.1, 
+                                             jitter.height = 0.1))
+
+
+# graph without counts
+ggplot(data = interviews_plotting,
+       aes(x=respondent_wall_type, y = liv_count,
+           fill=memb_assoc, color = memb_assoc)) +
+  geom_boxplot(alpha=0.5) +
+  geom_point(alpha = 0.5,
+             position = position_jitterdodge(jitter.width = 0.1, 
+                                             jitter.height = 0.1))
+  geom_text(aes(label=stat(count),y=5.5),
+            position = position_fill(), color = "white")
+
 
